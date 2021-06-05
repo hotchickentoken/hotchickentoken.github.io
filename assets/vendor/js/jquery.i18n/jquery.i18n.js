6f6e36a66e9e4937f8a875c4affe87ec6ff07007
maldiohead <maldiohead@outlook.com>
2021-06-05 18:52:12 +0800

    update
    Signed-off-by: maldiohead <maldiohead@outlook.com>

--- assets/vendor/js/jquery.i18n/jquery.i18n.js
+++ assets/vendor/js/jquery.i18n/jquery.i18n.js
@@ -273,7 +273,7 @@
 	/* Static members */
 	I18N.defaults = {
 		locale: String.locale,
-		fallbackLocale: 'zh',
+		fallbackLocale: 'en',
 		parser: $.i18n.parser,
 		messageStore: $.i18n.messageStore
 	};
